e55ef952be96d0c3540f34a513fed95fe09283ac
Teruki Goto <teruki.gotoh@gmall.com>
2021-02-13 15:54:09 +0900

    Modify CircleCI config

--- .circleci/scripts/is_changed.sh
+++ .circleci/scripts/is_changed.sh
@@ -6,6 +6,9 @@ else
   DIFF_TARGET="origin/develop"
 fi
 
+echo $(CIRCLE_BRANCH: ${CIRCLE_BRANCH})
+echo $(DIFF_SCRIPTS: git diff ${DIFF_TARGET} --name-only --relative=${1})
+
 DIFF_FILES=($(git diff ${DIFF_TARGET} --name-only --relative=${1}))
 
 if [ ${#DIFF_FILES[@]} -eq 0 ]; then
